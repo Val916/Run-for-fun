8d3a5b84e11677a05109ff9b27725bb12ead33d0
Val916 <junsofdev@proton.me>
2025-09-24 15:05:33 +0100

    Update settings for Cloudinary integration and add Whitenoise middleware; create requirements file and add initial CSS file.

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
     'cloudinary',
     'races',
@@ -51,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
